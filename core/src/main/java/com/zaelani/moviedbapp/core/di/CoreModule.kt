726d225b4b74409d872c1a15a7168063ab4157ea
zaelani23 <mohamadzaelani09@gmail.com>
2022-03-05 21:55:41 +0700

    security

--- core/src/main/java/com/zaelani/moviedbapp/core/di/CoreModule.kt
+++ core/src/main/java/com/zaelani/moviedbapp/core/di/CoreModule.kt
@@ -11,6 +11,9 @@ import com.zaelani.moviedbapp.core.domain.repository.IMovieRepository
 import com.zaelani.moviedbapp.core.domain.repository.ITvShowRepository
 import com.zaelani.moviedbapp.core.utils.AppExecutors
 import com.zaelani.moviedbapp.core.utils.NetworkInfo.BASE_URL
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -22,19 +25,30 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<MovieAppDatabase>().movieDBAppDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("cPvgsplHJgUEmXR".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             MovieAppDatabase::class.java, "moviedb.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
 val networkModule = module {
     single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/oD/WAoRPvbez1Y2dfYfuo4yujAcYHXdv1Ivb2v2MOKk=")
+            .add(hostname, "sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=")
+            .add(hostname, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
